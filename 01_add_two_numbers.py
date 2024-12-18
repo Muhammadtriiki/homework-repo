6a8869f19d23af07bb149d9e0104cd5b40244af5
Muhammadtriiki <122075613+Muhammadtriiki@users.noreply.github.com>
2024-12-18 20:11:09 +0500

    Add files via upload

---
+++ 01_add_two_numbers.py
@@ -0,0 +1,3 @@
+num1 = int(input("write the first number for addition"))
+num2 = int(input("write the second number for addition"))
+print(num1 + num2)
---
+++ 02_.aggreement_bot.py
@@ -0,0 +1,3 @@
+print("hello")
+favorite_animal = input("Whats your favorite animal ? " )
+print(f"My favorite animal is also {favorite_animal} !")
